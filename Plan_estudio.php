be32577d167589801e3b86f7b2fcc330c37ba8ee
Juan busqued <busquedjuan@gmail.com>
2026-06-16 16:17:03 -0300

    Cambiar el nombre de header.php a navbar.php y crear archivo header.php y eliminar todo el codigo inecesario header y modular todo

--- Plan_estudio.php
+++ Plan_estudio.php
@@ -1,51 +1,41 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Plan de Estudios</title>
-  <link rel="stylesheet" href="css/Plan_estudio.css">
-  <link rel="stylesheet" href="css/styles.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-  <link rel="icon" href="imagenes/escudo.png" type="image/png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plan de Estudios</title>
+
+    <link rel="stylesheet" href="css/Plan_estudio.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="icon" href="imagenes/escudo.png" type="image/png">
 </head>
 
-<header>
-    <div class="container">
-      <div class="header-content">
-        <div class="logo">
-          <a href="index.php">
-            <img src="imagenes/escudo.png" alt="Escudo de la Escuela Técnica">
-            <h1>Escuela Técnica N°1</h1>
-          </a>
-        </div>
-        
-        <div class="search-container">
-          <input type="text" placeholder="Buscar..." class="search-input">
-          <button class="search-button"><i class="fas fa-search"></i></button>
-        </div>
-      </div>
-    </div>
-</header>
-
 <body>
+
   <?php include 'header.php'; ?>
+
+  <?php include 'navbar.php'; ?>
+
+<main>
   <div class="container">
     <h1>Plan de Estudio</h1>
     <div class="controls">
-      <div class="year-selector">
-        <label for="year-select">Seleccionar Año:</label>
-        <select id="year-select">
-          <option value="1">1er Año</option>
-          <option value="2">2do Año</option>
-          <option value="3">3er Año</option>
-          <option value="4">4to Año</option>
-          <option value="5">5to Año</option>
-          <option value="6">6to Año</option>
-          <option value="7">7mo Año</option>
-        </select>
-      </div>
-      
+        <div class="year-selector">
+          <label for="year-select">Seleccionar Año:</label>
+            <select id="year-select">
+              <option value="1">1er Año</option>
+              <option value="2">2do Año</option>
+              <option value="3">3er Año</option>
+              <option value="4">4to Año</option>
+              <option value="5">5to Año</option>
+              <option value="6">6to Año</option>
+              <option value="7">7mo Año</option>
+            </select>
+        </div>
+
       <div class="specialty-selector" id="specialty-container">
         <label for="specialty-select">Especialidad:</label>
         <select id="specialty-select">
@@ -54,7 +44,7 @@
         </select>
       </div>
     </div>
-    
+
     <div class="schedule-container">
       <table id="plan-table" class="plan-table">
         <thead>
@@ -64,14 +54,19 @@
             <th>Total de Horas</th>
           </tr>
         </thead>
+
         <tbody id="plan-body">
-          <!-- El contenido de la tabla se generará dinámicamente con JavaScript (NO tocar nada)-->
+        <!-- El contenido se genera dinámicamente con JavaScript -->
         </tbody>
       </table>
     </div>
   </div>
+</main>
 
-  <script src="js/Plan_estudio.js"></script>
+    <?php include 'footer.php'; ?>
+
+    <script src="js/Plan_estudio.js"></script>
+
 </body>
-<?php include 'footer.php'; ?>
+
 </html>
--- egresados.php
+++ egresados.php
@@ -13,24 +13,10 @@
 
 </head>
 <body>
-    <header>
-        <div class="container">
-            <div class="header-content">
-                <div class="logo">
-                    <a href="index.php">
-                        <img src="imagenes/escudo.png" alt="Escudo de la Escuela Técnica">
-                        <h1>Escuela Técnica N°1</h1>
-                    </a>
-                </div>
-                <div class="search-container">
-                    <input type="text" placeholder="Buscar..." class="search-input">
-                    <button class="search-button"><i class="fas fa-search"></i></button>
-                </div>
-            </div>
-        </div>
-    </header>
     
-    <?php include 'header.php'; ?>
+    <<?php include 'header.php'; ?>
+
+    <?php include 'navbar.php'; ?>
 
     <div class="search-container-sw">
         <input type="text"
